ab5cbdcb11119a4f7a4800b076fca5d66b1da782
SeanAC <sean.critica@gmail.com>
2014-01-09 17:54:03 +0800

    Starting with custom overlay view.

--- src/com/sean/android/utils/thumbtype/ThumbType.java
+++ src/com/sean/android/utils/thumbtype/ThumbType.java
@@ -16,423 +16,98 @@
 
 package com.sean.android.utils.thumbtype;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import android.graphics.PixelFormat;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.KeyboardView;
-import android.text.InputType;
-import android.text.method.MetaKeyKeyListener;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.CompletionInfo;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputConnection;
 
 public class ThumbType extends InputMethodService 
         implements KeyboardView.OnKeyboardActionListener {
+	
     static final boolean DEBUG = false;
     
-    /**
-     * This boolean indicates the optional example code for performing
-     * processing of hard keys in addition to regular text generation
-     * from on-screen interaction.  It would be used for input methods that
-     * perform language translations (such as converting text entered on 
-     * a QWERTY keyboard to Chinese), but may not be used for input methods
-     * that are primarily intended to be used for on-screen text entry.
-     */
-    static final boolean PROCESS_HARD_KEYS = true;
-
-    private ThumbTypeView mInputView;
-    private CandidateView mCandidateView;
-    private CompletionInfo[] mCompletions;
+    private WindowManager wm;
+    private WindowManager.LayoutParams wmParams;
+    private ThumbTypeView inputView;
     
-    private StringBuilder mComposing = new StringBuilder();
-    private boolean mPredictionOn;
-    private boolean mCompletionOn;
-    private int mLastDisplayWidth;
-    private long mMetaState;
-    
-    private LatinKeyboard mSymbolsKeyboard;
-    private LatinKeyboard mQwertyKeyboard;
-    
-    private LatinKeyboard mCurKeyboard;
-    
-    private String mWordSeparators;
-    
-    /**
-     * Main initialization of the input method component.  Be sure to call
-     * to super class.
-     */
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
-        mWordSeparators = getResources().getString(R.string.word_separators);
+        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        wmParams = new WindowManager.LayoutParams(
+        					WindowManager.LayoutParams.WRAP_CONTENT,
+        					WindowManager.LayoutParams.WRAP_CONTENT,
+        					WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+        					0,
+        					PixelFormat.TRANSLUCENT);
+        inputView = (ThumbTypeView) getLayoutInflater().inflate(R.layout.thumb_type_view, null);
     }
     
-    /**
-     * This is the point where you can do all of your UI initialization.  It
-     * is called after creation and any configuration change.
-     */
-    @Override public void onInitializeInterface() {
-        if (mQwertyKeyboard != null) {
-            // Configuration changes can happen after the keyboard gets recreated,
-            // so we need to be able to re-build the keyboards if the available
-            // space has changed.
-            int displayWidth = getMaxWidth();
-            if (displayWidth == mLastDisplayWidth) return;
-            mLastDisplayWidth = displayWidth;
-        }
-        mQwertyKeyboard = new LatinKeyboard(this, R.xml.qwerty);
-        mSymbolsKeyboard = new LatinKeyboard(this, R.xml.symbols);
+    @Override
+    public void onInitializeInterface() {
+    }
+
+    @Override
+    public View onCreateInputView() {
+    	return null;
     }
     
-    /**
-     * Called by the framework when your view for creating input needs to
-     * be generated.  This will be called the first time your input method
-     * is displayed, and every time it needs to be re-created such as due to
-     * a configuration change.
-     */
-    @Override public View onCreateInputView() {
-        mInputView = (ThumbTypeView) getLayoutInflater().inflate(R.layout.thumb_type_view, null);
-        return mInputView;
+    @Override
+    public void onStartInputView(EditorInfo attribute, boolean restarting) {
+       	if (inputView != null)
+    		wm.addView(inputView, wmParams);
+    	super.onStartInputView(attribute, restarting);
+    }
+    
+    @Override
+    public void onFinishInputView(boolean finishingInput) {
+    	if (inputView != null)
+    		wm.removeView(inputView);
+    	super.onFinishInputView(finishingInput);
     }
 
-    @Override public boolean onEvaluateFullscreenMode() {
-    	return true;
-    }
-
-    /**
-     * This is the main point where we do our initialization of the input method
-     * to begin operating on an application.  At this point we have been
-     * bound to the client, and are now receiving all of the detailed information
-     * about the target of our edits.
-     */
-    @Override public void onStartInput(EditorInfo attribute, boolean restarting) {
+    @Override
+    public void onStartInput(EditorInfo attribute, boolean restarting) {
         super.onStartInput(attribute, restarting);
-        
-        // Reset our state.  We want to do this even if restarting, because
-        // the underlying state of the text editor could have changed in any way.
-        mComposing.setLength(0);
-        updateCandidates();
-        
-        if (!restarting) {
-            // Clear shift states.
-            mMetaState = 0;
-        }
-        
-        mPredictionOn = false;
-        mCompletionOn = false;
-        mCompletions = null;
-        
-        // We are now going to initialize our state based on the type of
-        // text being edited.
-        switch (attribute.inputType & InputType.TYPE_MASK_CLASS) {
-            case InputType.TYPE_CLASS_NUMBER:
-            case InputType.TYPE_CLASS_DATETIME:
-                // Numbers and dates default to the symbols keyboard, with
-                // no extra features.
-                mCurKeyboard = mSymbolsKeyboard;
-                break;
-                
-            case InputType.TYPE_CLASS_PHONE:
-                // Phones will also default to the symbols keyboard, though
-                // often you will want to have a dedicated phone keyboard.
-                mCurKeyboard = mSymbolsKeyboard;
-                break;
-                
-            case InputType.TYPE_CLASS_TEXT:
-                // This is general text editing.  We will default to the
-                // normal alphabetic keyboard, and assume that we should
-                // be doing predictive text (showing candidates as the
-                // user types).
-                mCurKeyboard = mQwertyKeyboard;
-                mPredictionOn = true;
-                
-                // We now look for a few special variations of text that will
-                // modify our behavior.
-                int variation = attribute.inputType & InputType.TYPE_MASK_VARIATION;
-                if (variation == InputType.TYPE_TEXT_VARIATION_PASSWORD ||
-                        variation == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
-                    // Do not display predictions / what the user is typing
-                    // when they are entering a password.
-                    mPredictionOn = false;
-                }
-                
-                if (variation == InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
-                        || variation == InputType.TYPE_TEXT_VARIATION_URI
-                        || variation == InputType.TYPE_TEXT_VARIATION_FILTER) {
-                    // Our predictions are not useful for e-mail addresses
-                    // or URIs.
-                    mPredictionOn = false;
-                }
-                
-                if ((attribute.inputType & InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE) != 0) {
-                    // If this is an auto-complete text view, then our predictions
-                    // will not be shown and instead we will allow the editor
-                    // to supply their own.  We only show the editor's
-                    // candidates when in fullscreen mode, otherwise relying
-                    // own it displaying its own UI.
-                    mPredictionOn = false;
-                    mCompletionOn = isFullscreenMode();
-                }
-                
-                // We also want to look at the current state of the editor
-                // to decide whether our alphabetic keyboard should start out
-                // shifted.
-                updateShiftKeyState(attribute);
-                break;
-                
-            default:
-                // For all unknown input types, default to the alphabetic
-                // keyboard with no special features.
-                mCurKeyboard = mQwertyKeyboard;
-                updateShiftKeyState(attribute);
-        }
-        
-        // Update the label on the enter key, depending on what the application
-        // says it will do.
-        mCurKeyboard.setImeOptions(getResources(), attribute.imeOptions);
     }
 
-    /**
-     * This is called when the user is done editing a field.  We can use
-     * this to reset our state.
-     */
-    @Override public void onFinishInput() {
+    @Override
+    public void onFinishInput() {
         super.onFinishInput();
-        
-        // Clear current composing text and candidates.
-        mComposing.setLength(0);
-        updateCandidates();
-        
-        // We only hide the candidates window when finishing input on
-        // a particular editor, to avoid popping the underlying application
-        // up and down if the user is entering text into the bottom of
-        // its window.
-        setCandidatesViewShown(false);
-        mCurKeyboard = mQwertyKeyboard;
     }
 
-    @Override public void onStartInputView(EditorInfo attribute, boolean restarting) {
-        super.onStartInputView(attribute, restarting);
-    }
+	@Override
+	public void onPress(int primaryCode) {
+	}
 
-    /**
-     * Deal with the editor reporting movement of its cursor.
-     */
-    @Override public void onUpdateSelection(int oldSelStart, int oldSelEnd,
-            int newSelStart, int newSelEnd,
-            int candidatesStart, int candidatesEnd) {
-        super.onUpdateSelection(oldSelStart, oldSelEnd, newSelStart, newSelEnd,
-                candidatesStart, candidatesEnd);
-        
-        // If the current selection in the text view changes, we should
-        // clear whatever candidate text we have.
-        if (mComposing.length() > 0 && (newSelStart != candidatesEnd
-                || newSelEnd != candidatesEnd)) {
-            mComposing.setLength(0);
-            updateCandidates();
-            InputConnection ic = getCurrentInputConnection();
-            if (ic != null) {
-                ic.finishComposingText();
-            }
-        }
-    }
+	@Override
+	public void onRelease(int primaryCode) {
+	}
 
-    /**
-     * This tells us about completions that the editor has determined based
-     * on the current text in it.  We want to use this in fullscreen mode
-     * to show the completions ourself, since the editor can not be seen
-     * in that situation.
-     */
-    @Override public void onDisplayCompletions(CompletionInfo[] completions) {
-        if (mCompletionOn) {
-            mCompletions = completions;
-            if (completions == null) {
-                setSuggestions(null, false, false);
-                return;
-            }
-            
-            List<String> stringList = new ArrayList<String>();
-            for (int i = 0; i < completions.length; i++) {
-                CompletionInfo ci = completions[i];
-                if (ci != null) stringList.add(ci.getText().toString());
-            }
-            setSuggestions(stringList, true, true);
-        }
-    }
-      
-    /**
-     * Use this to monitor key events being delivered to the application.
-     * We get first crack at them, and can either resume them or let them
-     * continue to the app.
-     */
-    @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return super.onKeyDown(keyCode, event);
-    }
+	@Override
+	public void onKey(int primaryCode, int[] keyCodes) {
+	}
 
-    /**
-     * Use this to monitor key events being delivered to the application.
-     * We get first crack at them, and can either resume them or let them
-     * continue to the app.
-     */
-    @Override public boolean onKeyUp(int keyCode, KeyEvent event) {
-        // If we want to do transformations on text being entered with a hard
-        // keyboard, we need to process the up events to update the meta key
-        // state we are tracking.
-        if (PROCESS_HARD_KEYS) {
-            if (mPredictionOn) {
-                mMetaState = MetaKeyKeyListener.handleKeyUp(mMetaState,
-                        keyCode, event);
-            }
-        }
-        
-        return super.onKeyUp(keyCode, event);
-    }
+	@Override
+	public void onText(CharSequence text) {
+	}
 
-    /**
-     * Helper function to commit any text being composed in to the editor.
-     */
-    private void commitTyped(InputConnection inputConnection) {
-        if (mComposing.length() > 0) {
-            inputConnection.commitText(mComposing, mComposing.length());
-            mComposing.setLength(0);
-            updateCandidates();
-        }
-    }
+	@Override
+	public void swipeLeft() {
+	}
 
-    /**
-     * Helper to update the shift state of our keyboard based on the initial
-     * editor state.
-     */
-    private void updateShiftKeyState(EditorInfo attr) {
-    }
-    
-    /**
-     * Helper to send a key down / key up pair to the current editor.
-     */
-    private void keyDownUp(int keyEventCode) {
-        getCurrentInputConnection().sendKeyEvent(
-                new KeyEvent(KeyEvent.ACTION_DOWN, keyEventCode));
-        getCurrentInputConnection().sendKeyEvent(
-                new KeyEvent(KeyEvent.ACTION_UP, keyEventCode));
-    }
-    
-    public void onKey(int primaryCode, int[] keyCodes) {
-    }
+	@Override
+	public void swipeRight() {
+	}
 
-    public void onText(CharSequence text) {
-        InputConnection ic = getCurrentInputConnection();
-        if (ic == null) return;
-        ic.beginBatchEdit();
-        if (mComposing.length() > 0) {
-            commitTyped(ic);
-        }
-        ic.commitText(text, 0);
-        ic.endBatchEdit();
-        updateShiftKeyState(getCurrentInputEditorInfo());
-    }
+	@Override
+	public void swipeDown() {
+	}
 
-    /**
-     * Update the list of available candidates from the current composing
-     * text.  This will need to be filled in by however you are determining
-     * candidates.
-     */
-    private void updateCandidates() {
-        if (!mCompletionOn) {
-            if (mComposing.length() > 0) {
-                ArrayList<String> list = new ArrayList<String>();
-                list.add(mComposing.toString());
-                setSuggestions(list, true, true);
-            } else {
-                setSuggestions(null, false, false);
-            }
-        }
-    }
-    
-    public void setSuggestions(List<String> suggestions, boolean completions,
-            boolean typedWordValid) {
-        if (suggestions != null && suggestions.size() > 0) {
-            setCandidatesViewShown(true);
-        } else if (isExtractViewShown()) {
-            setCandidatesViewShown(true);
-        }
-        if (mCandidateView != null) {
-            mCandidateView.setSuggestions(suggestions, completions, typedWordValid);
-        }
-    }
-    
-    private void handleBackspace() {
-        final int length = mComposing.length();
-        if (length > 1) {
-            mComposing.delete(length - 1, length);
-            getCurrentInputConnection().setComposingText(mComposing, 1);
-            updateCandidates();
-        } else if (length > 0) {
-            mComposing.setLength(0);
-            getCurrentInputConnection().commitText("", 0);
-            updateCandidates();
-        } else {
-            keyDownUp(KeyEvent.KEYCODE_DEL);
-        }
-        updateShiftKeyState(getCurrentInputEditorInfo());
-    }
+	@Override
+	public void swipeUp() {
+	}
 
-    private void handleClose() {
-        commitTyped(getCurrentInputConnection());
-        requestHideSelf(0);
-    }
-    
-    private String getWordSeparators() {
-        return mWordSeparators;
-    }
-    
-    public boolean isWordSeparator(int code) {
-        String separators = getWordSeparators();
-        return separators.contains(String.valueOf((char)code));
-    }
-
-    public void pickDefaultCandidate() {
-        pickSuggestionManually(0);
-    }
-    
-    public void pickSuggestionManually(int index) {
-        if (mCompletionOn && mCompletions != null && index >= 0
-                && index < mCompletions.length) {
-            CompletionInfo ci = mCompletions[index];
-            getCurrentInputConnection().commitCompletion(ci);
-            if (mCandidateView != null) {
-                mCandidateView.clear();
-            }
-            updateShiftKeyState(getCurrentInputEditorInfo());
-        } else if (mComposing.length() > 0) {
-            // If we were generating candidate suggestions for the current
-            // text, we would commit one of them here.  But for this sample,
-            // we will just commit the current text.
-            commitTyped(getCurrentInputConnection());
-        }
-    }
-    
-    public void swipeRight() {
-        if (mCompletionOn) {
-            pickDefaultCandidate();
-        }
-    }
-    
-    public void swipeLeft() {
-        handleBackspace();
-    }
-
-    public void swipeDown() {
-        handleClose();
-    }
-
-    public void swipeUp() {
-    }
-    
-    public void onPress(int primaryCode) {
-    }
-    
-    public void onRelease(int primaryCode) {
-    }
 }
--- src/com/sean/android/utils/thumbtype/ThumbTypeView.java
+++ src/com/sean/android/utils/thumbtype/ThumbTypeView.java
@@ -1,28 +1,86 @@
 package com.sean.android.utils.thumbtype;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.WindowManager;
 
-public class ThumbTypeView extends View implements OnClickListener {
+public class ThumbTypeView extends View {
 
+	private Context cnxt;
+	private WindowManager wm;
+	private Display disp;
+	
+	private Point screenSize = new Point();
+	
+	private ShapeDrawable leftControl;
+	private Rect leftControlBounds;
+	
+	private ShapeDrawable rightControl;
+	private Rect rightControlBounds;
+	
 	public ThumbTypeView(Context context) {
 		super(context);
+		initialize(context);
 	}
 	
 	public ThumbTypeView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		initialize(context);
 	}
 
 	public ThumbTypeView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		initialize(context);
+	}
+
+	private void initialize(Context context) {
+		cnxt = context;
+		getDisplayParameters();
+		initializeGraphicsObjects();
+	}
+	
+	private void getDisplayParameters() {
+		wm = (WindowManager) cnxt.getSystemService(Context.WINDOW_SERVICE);
+		disp = wm.getDefaultDisplay();
+		disp.getSize(screenSize);
+	}
+	
+	private void initializeGraphicsObjects() {
+		initializeLeftControl();
+		initializeRightControl();
+	}
+
+	private void initializeLeftControl() {
+		leftControl = new ShapeDrawable(new OvalShape());
+		leftControl.getPaint().setColor(0xFF0055FF);
+		int left = screenSize.x / 20;
+		int bottom = screenSize.x / 20 * 19;
+		int diameter = 100;
+		leftControlBounds = new Rect(left, bottom - diameter, left + diameter, bottom);
+		leftControl.setBounds(leftControlBounds);
+	}
+
+	private void initializeRightControl() {
+		rightControl = new ShapeDrawable(new OvalShape());
+		rightControl.getPaint().setColor(0xFF0055FF);
+		int right = screenSize.x / 20 * 19;
+		int bottom = screenSize.x / 20 * 19;
+		int diameter = 100;
+		rightControlBounds = new Rect(right - diameter, bottom - diameter, right + diameter, bottom);
+		rightControl.setBounds(rightControlBounds);
 	}
 
 	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onDraw(Canvas canvas) {
+		leftControl.draw(canvas);
+		rightControl.draw(canvas);
 	}
-
+	
 }
